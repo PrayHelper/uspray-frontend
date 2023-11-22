a4907539c3d10f65f0ec73509bd4f84cc03f8fb1
alicehrk <alicehrk23@gmail.com>
2023-11-22 19:52:55 +0900

    refactor: MainContent.js 리팩토링

--- src/components/Main/MainContent.js
+++ src/components/Main/MainContent.js
@@ -4,23 +4,29 @@ import Category from "../Category/Category";
 import CategoryTag from "../CategoryTag/CategoryTag";
 
 const MainContent = ({categories, setCategories, setShowCategorySetting}) => {
-
   const [selectedCategoryIndex, setSelectedCategoryIndex] = useState(null);
-
+  
   return (
     <MainContentWrapper>
-      <div style={{display: "flex", padding: "24px 16px 16px 16px", position: "sticky", top: "0", backgroundColor: "rgba(255, 255, 255, 0.85)", borderRadius: "24px 24px 0 0", zIndex: "50", backdropFilter: "blur(12px)"}}>
-        <CategoryTag categories={categories} setCategories={setCategories} selectedCategoryIndex={selectedCategoryIndex} setSelectedCategoryIndex={setSelectedCategoryIndex} setShowCategorySetting={setShowCategorySetting} />
-      </div>
-      <div style={{display: "flex", flexDirection: "column", gap: "24px", padding: "8px 16px 60px 16px"}}>
+      <TopWrapper>
+        <CategoryTag 
+          categories={categories} 
+          setCategories={setCategories} 
+          selectedCategoryIndex={selectedCategoryIndex} 
+          setSelectedCategoryIndex={setSelectedCategoryIndex} 
+          setShowCategorySetting={setShowCategorySetting} 
+        />
+      </TopWrapper>
+      <Content>
         <Category title="테스트" color="#75BD62" />
         <Category title="테스트2" color="#AEDBA5" />
-      </div>
+      </Content>
     </MainContentWrapper>
   );
 };
 
 export default MainContent;
+
 
 const MainContentWrapper = styled.div`
   display: flex;
@@ -32,13 +38,27 @@ const MainContentWrapper = styled.div`
   border-radius: 32px 32px 0px 0px;
   box-sizing: border-box;
   overflow-y: auto;
-
-  // Chrome, Edge 등
   &::-webkit-scrollbar {
     display: none;
   }
-  // Firefox
   scrollbar-width: none;
-  // IE, Edge
   -ms-overflow-style: none;
+`;
+
+const TopWrapper = styled.div`
+  display: flex;
+  padding: 24px 16px 16px;
+  position: sticky;
+  top: 0;
+  background-color: rgba(255, 255, 255, 0.85);
+  border-radius: 24px 24px 0 0;
+  z-index: 50;
+  backdrop-filter: blur(12px);
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  padding: 8px 16px 60px;
 `;
