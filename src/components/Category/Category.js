c02895cdb978b5e3cbe77da0c01481377d70998e
alicehrk <alicehrk23@gmail.com>
2023-11-22 19:59:58 +0900

    refactor: Category.js 리팩토링

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import styled from "styled-components";
 
+const ICON_HEART_FILLED = "images/ic_filled_heart.svg";
+const ICON_HEART_EMPTY = "images/ic_empty_heart.svg";
+
 const Category = ({title, color}) => {
   const [selected, setSelected] = useState([]);
   const titles = ['기도제목1', '기도제목2', '기도제목3'];
@@ -14,23 +17,50 @@ const Category = ({title, color}) => {
     });
   };
 
-
   return (
-    <div style={{display: "flex", flexDirection: "column", width: "100%", minHeight: "100px",borderRadius: "16px", boxShadow: "0px 2px 8px 0px rgba(0, 0, 0, 0.25)"}}>
-      <div style={{backgroundColor: `${color}`, borderRadius: "16px 16px 0px 0px", padding: "12px 16px", color: "#FFFFFF", fontWeight: "700"}}>{title}</div>
-      <div style={{display: "flex", flexDirection: "column", }}>
+    <CategoryContainer>
+      <Title color={color}>{title}</Title>
+      <ItemList>
       {titles.map((title, index) => (
         <Item key={index} >
-          <div style={{flex: "1 1 0%", color: selected[index] ? "#49614380" : "#496143", fontSize: "12px"}}>{title}</div>
-          <img src={selected[index] ? "images/ic_filled_heart.svg" : "images/ic_empty_heart.svg"} alt="heart_icon" onClick={(e) => handleClick(e, index)}/>
+          <ItemText selected={selected[index]}>{title}</ItemText>
+          <img src={selected[index] ? ICON_HEART_FILLED : ICON_HEART_EMPTY} alt="heart_icon" onClick={(e) => handleClick(e, index)}/>
         </Item>
       ))}
-      </div>
-    </div>
+      </ItemList>
+    </CategoryContainer>
   );
 };
 
 export default Category;
+
+const CategoryContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  min-height: 100px;
+  border-radius: 16px;
+  box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, 0.25);
+`;
+
+const Title = styled.div`
+  background-color: ${props => props.color};
+  border-radius: 16px 16px 0px 0px;
+  padding: 12px 16px;
+  color: #FFFFFF;
+  font-weight: 700;
+`;
+
+const ItemList = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ItemText = styled.div`
+  flex: 1 1 0%;
+  color: ${props => props.selected ? "#49614380" : "#496143"};
+  font-size: 12px;
+`;
 
 const Item = styled.div`
   padding: 16px;
