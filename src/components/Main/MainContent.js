7bf77ec89c0563ca1f79de82c1bacda11d956e9d
graS2e <77924292+graS2e@users.noreply.github.com>
2023-12-10 20:52:22 +0900

    fix: merge conflict 해결

--- src/components/Main/MainContent.js
+++ src/components/Main/MainContent.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import styled from "styled-components";
 import Category from "../Category/Category";
-
+import CategoryTag from "../CategoryTag/CategoryTag";
 import completeImage from "../../images/check_img.svg";
 import deleteImage from "../../images/delete_img.svg";
 import modifyImage from "../../images/modify_img.svg";
@@ -10,18 +10,17 @@ const MainContent = ({ categories, setCategories, setShowCategorySetting }) => {
   const [selectedCategoryIndex, setSelectedCategoryIndex] = useState(null);
   const [selectedTitleIndex, setSelectedTitleIndex] = useState(null);
 
-
   const prayComplete = () => {
     // 기도 완료하는 api
-  }
+  };
 
   const prayModify = () => {
     // 기도 수정하는 api
-  }
+  };
 
   const prayDeleted = () => {
     // 기도 삭제하는 api
-  }
+  };
   return (
     <MainContentWrapper>
       <TopWrapper>
@@ -34,30 +33,46 @@ const MainContent = ({ categories, setCategories, setShowCategorySetting }) => {
         />
       </TopWrapper>
       <Content>
-        <Category title="테스트" color="#75BD62" setSelectedTitleIndex={setSelectedTitleIndex} />
-        <Category title="테스트2" color="#AEDBA5" setSelectedTitleIndex={setSelectedTitleIndex} />
+        <Category
+          title="테스트"
+          color="#75BD62"
+          setSelectedTitleIndex={setSelectedTitleIndex}
+        />
+        <Category
+          title="테스트2"
+          color="#AEDBA5"
+          setSelectedTitleIndex={setSelectedTitleIndex}
+        />
       </Content>
       <BottomSetWrapper selectedTitleIndex={selectedTitleIndex}>
         <BottomButtonWrapper>
           <img src={completeImage} />
-          <BottomButtonText color={"green"} onClick={() => prayComplete()}>완료하기</BottomButtonText>
+          <BottomButtonText color={"green"} onClick={() => prayComplete()}>
+            완료하기
+          </BottomButtonText>
         </BottomButtonWrapper>
         <BottomButtonWrapper>
           <img src={modifyImage} />
-          <BottomButtonText color={"blue"} onClick={() => prayModify()}>수정하기</BottomButtonText>
+          <BottomButtonText color={"blue"} onClick={() => prayModify()}>
+            수정하기
+          </BottomButtonText>
         </BottomButtonWrapper>
         <BottomButtonWrapper>
           <img src={deleteImage} />
-          <BottomButtonText color={"red"} onClick={() => prayDeleted()}>삭제하기</BottomButtonText>
+          <BottomButtonText color={"red"} onClick={() => prayDeleted()}>
+            삭제하기
+          </BottomButtonText>
         </BottomButtonWrapper>
       </BottomSetWrapper>
-      <BlackBackground selectedTitleIndex={selectedTitleIndex} onClick={() => setSelectedTitleIndex(null)} />
+      <BlackBackground
+        selectedTitleIndex={selectedTitleIndex}
+        onClick={() => setSelectedTitleIndex(null)}
+      />
     </MainContentWrapper>
   );
 };
 
 export default MainContent;
-
 
 const MainContentWrapper = styled.div`
   display: flex;
@@ -97,45 +112,51 @@ const Content = styled.div`
 const BottomSetWrapper = styled.div`
   display: flex;
   position: fixed;
-  box-sizing : border-box;
-  justify-content : center;
-  gap : 18px;
+  box-sizing: border-box;
+  justify-content: center;
+  gap: 18px;
   width: 100%;
   padding: 37px 24px;
-  transition : all 0.3s ease-in-out;
-  bottom: ${(props) => props.selectedTitleIndex == null ? "-100%" : "0px"};
-  z-index : 101;
+  transition: all 0.3s ease-in-out;
+  bottom: ${(props) => (props.selectedTitleIndex == null ? "-100%" : "0px")};
+  z-index: 101;
   border-radius: 24px 24px 0px 0px;
-  background: #FFF;
-`
+  background: #fff;
+`;
 
 const BottomButtonText = styled.div`
   font-size: 16px;
   font-style: normal;
   font-weight: 700;
   line-height: normal;
-  color : ${(props) => props.color == "green" ? "#27CD2F" : (props.color == "blue") ? "#408CFF" : "#FF4F4F"};
-`
+  color: ${(props) =>
+    props.color == "green"
+      ? "#27CD2F"
+      : props.color == "blue"
+      ? "#408CFF"
+      : "#FF4F4F"};
+`;
 
 const BottomButtonWrapper = styled.button`
   flex-grow: 1;
-  padding : 12px 28px;
-  border-radius : 16px;
-  background : #F8F8F8;
+  padding: 12px 28px;
+  border-radius: 16px;
+  background: #f8f8f8;
   border: none;
-`
+`;
 
 const BlackBackground = styled.div`
-  transition : all 0.3s ease-in-out;
+  transition: all 0.3s ease-in-out;
   background-color: rgba(0, 0, 0, 0.7);
-  display : flex;
-  position : fixed;
-  top : 0;
-  right : 0;
-  left : 0;
-  bottom : 0;
-  z-index : ${(props) => props.selectedTitleIndex !== null ? 100 : 0};
-  opacity : ${(props) => props.selectedTitleIndex !== null ? 1 : 0};
-  backdrop-filter : blur(4px);
-  pointer-events : ${(props) => props.selectedTitleIndex !== null ? "auto" : "none"};
-`
+  display: flex;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  z-index: ${(props) => (props.selectedTitleIndex !== null ? 100 : 0)};
+  opacity: ${(props) => (props.selectedTitleIndex !== null ? 1 : 0)};
+  backdrop-filter: blur(4px);
+  pointer-events: ${(props) =>
+    props.selectedTitleIndex !== null ? "auto" : "none"};
+`;
