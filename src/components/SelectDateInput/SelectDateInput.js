7d36a072eaa5a65ef1f347248859493332485ed2
graS2e <77924292+graS2e@users.noreply.github.com>
2023-12-12 19:12:07 +0900

    wip

--- src/components/SelectDateInput/SelectDateInput.js
+++ src/components/SelectDateInput/SelectDateInput.js
@@ -4,18 +4,19 @@ import { useRef, useState } from "react";
 import styled from "styled-components";
 import BlackScreen from "../BlackScreen/BlackScreen";
 
-/*
-  props 넘겨받을 목록 (History.js 파일 참고하기)
-  1. maxlen : 최대 길이, maxrow : 최대 줄바꿈, inputPlaceHolder
-  2. setUpdateDate 변수 (api 호출용 날짜 데이터 저장)
-  3. setUpdateValue 변수 (api 호출용 input 내용 데이터 저장)
-  4. showSubModal setShowSubModal 변수 (현재 컴포넌트 창 켜져있는지)
-  5. onClickFunc 기도 추가 이벤트 함수
-  6. isDefault 변수 (디폴트 값 존재하는지)
-  7. isShowWordCount 변수 (글자수 유무)
-*/
-
-const SelectDateInput = (props) => {
+const SelectDateInput = ({
+  setUpdateValue, // api 호출용 input 내용 데이터 저장
+  showSubModal, // 현재 컴포넌트 창 켜져있는지
+  setShowSubModal, // 현재 컴포넌트 창 켜져있는지 set
+  inputPlaceHolder,
+  maxrow, // 최대 줄바꿈
+  maxlen, // 최대 길이
+  isDefault, // 디폴트 값 존재하는지
+  isShowWordCount, // 글자수 유무
+  value,
+  setUpdateDate, // api 호출용 날짜 데이터 저장
+  onClickFunc, // 기도 추가 이벤트 함수
+}) => {
   const outside = useRef();
 
   const [inputCount, setInputCount] = useState(0);
@@ -24,47 +25,45 @@ const SelectDateInput = (props) => {
     if (e.target.value.length > e.maxLength)
       setInputCount(e.value.slice(0, e.maxLength));
     setInputCount(e.target.value.length);
-    if (props.setUpdateValue) props.setUpdateValue(e.target.value);
+    if (setUpdateValue) setUpdateValue(e.target.value);
   };
 
   return (
     <>
-      <BlackScreen isModalOn={props.showSubModal} zindex={400} />
+      <BlackScreen isModalOn={showSubModal} zindex={400} />
       <SubModalWrapper
-        showSubModal={props.showSubModal}
+        showSubModal={showSubModal}
         ref={outside}
         onClick={(e) => {
-          if (e.target === outside.current) props.setShowSubModal(false);
+          if (e.target === outside.current) setShowSubModal(false);
         }}
       >
         <SubModalTop>
           <ModalInputWrapper>
             <ModalInput
-              placeholder={props.inputPlaceHolder}
-              maxRows={props.maxrow}
+              placeholder={inputPlaceHolder}
+              maxRows={maxrow}
               minRows={1}
               cacheMeasurements
-              maxLength={props.maxlen}
+              maxLength={maxlen}
               onChange={onInputHandler}
-              disabled={props.isDefault ? true : false}
-              value={
-                props.isDefault ? "기도제목을 입력하였습니다." : props.value
-              }
+              disabled={isDefault ? true : false}
+              value={isDefault ? "기도제목을 입력하였습니다." : value}
             />
-            {props.isShowWordCount && (
+            {isShowWordCount && (
               <Countwords>
                 <p>
-                  {inputCount}자 / {props.maxlen}자
+                  {inputCount}자 / {maxlen}자
                 </p>
               </Countwords>
             )}
           </ModalInputWrapper>
           <SelectDate
-            setUpdateDate={props.setUpdateDate}
-            showSubModal={props.showSubModal}
+            setUpdateDate={setUpdateDate}
+            showSubModal={showSubModal}
           />
         </SubModalTop>
-        <SubModalBottom onClick={props.onClickFunc}>
+        <SubModalBottom onClick={onClickFunc}>
           오늘의 기도에 추가하기
         </SubModalBottom>
       </SubModalWrapper>
