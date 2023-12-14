73b77b459fcccab73953f18b41e67f16668c59f1
graS2e <77924292+graS2e@users.noreply.github.com>
2023-12-15 00:04:49 +0900

    fix: 오류 수정

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import MainContent from "../components/Main/MainContent";
 import { useState } from "react";
