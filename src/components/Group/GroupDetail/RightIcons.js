540466472c0579e634f6b1b9cbb32cd295b709ac
subin <200516bb@gmail.com>
2023-12-21 21:06:06 +0900

    feat: 모임 떠나기 api 연결

--- src/components/Group/GroupDetail/RightIcons.js
+++ src/components/Group/GroupDetail/RightIcons.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const RightIcons = ({group, setShow}) => {
+  const navigate = useNavigate();
   const [noticeOn, setNoticeOn] = useState(true);
   const isLeader = group.leader;
   return (
@@ -20,7 +22,7 @@ const RightIcons = ({group, setShow}) => {
           isLeader ?
             <img src='images/ic_group_setting.svg' alt='group_setting_icon' onClick={() => setShow(prev => !prev)}/>
             :
-            <img src='images/ic_group_leave.svg' alt='group_leave_icon'/>
+            <img src='images/ic_group_leave.svg' alt='group_leave_icon' onClick={() => navigate('/leaveGroup', { state: {groupId: group.id} })}/>
         }
       </div>
     </Wrapper>
