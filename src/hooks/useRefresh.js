68ec99b58af3ee1014ae32130997342d8c18a13c
graS2e <77924292+graS2e@users.noreply.github.com>
2023-12-21 16:35:08 +0900

    fix: reissue 안 되는 문제 해결

--- src/hooks/useRefresh.js
+++ src/hooks/useRefresh.js
@@ -7,37 +7,37 @@ const useRefresh = () => {
   const { getRefreshToken, setRefreshToken, setAccessToken } = useAuthToken();
   const { setUnAuthorized } = useAuthorized();
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   // accessToken 재발급을 위한 axios 호출
   const refresh = async () => {
-    console.log("Refresh는 실행이 되나?")
-  try {
-    const refreshToken = await getRefreshToken();
-    const res = await publicapi.post('/auth/reissue', null, {
-      headers: {
-        Refresh: `${refreshToken}`,
+    console.log("Refresh는 실행이 되나?");
+    try {
+      const refreshToken = await getRefreshToken();
+      const res = await publicapi.post("/auth/reissue", null, {
+        headers: {
+          Refresh: `${refreshToken}`,
+        },
+      });
+      console.log(`access_token: ${res.data.data.accessToken}`);
+      console.log(`refresh_token: ${res.data.data.refreshToken}`);
+      setAccessToken(res.data.data.accessToken);
+      setRefreshToken(res.data.data.refreshToken);
+    } catch (e) {
+      console.log("catch에서 실행");
+      // 401 : refresh token 만료
+      if (e.status === 401) {
+        await setRefreshToken("");
+        setUnAuthorized();
+        navigate("/");
       }
-    });
-    console.log(`access_token: ${res.data.data.accessToken}`);
-    setAccessToken(res.data.data.accessToken);
-
-  } catch (e) {
-    console.log("catch에서 실행");
-    // 401 : refresh token 만료
-    if (e.status === 401) {
-      await setRefreshToken('');
-      setUnAuthorized()
-      navigate("/")
+      throw e;
     }
-    throw e;
-  }
-};
+  };
 
   return {
-    refresh
-  }
-
-}
+    refresh,
+  };
+};
 
 export default useRefresh;
