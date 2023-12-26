ebfd928da3c97a4e8740aec0b60b3ccd5cf6dc35
limeojin363 <2002eojin@gmail.com>
2023-12-26 18:09:05 +0900

    feat: input에 fowardRef 지정, 회원가입에서 이름 모달 open시 blur / close시 re-focus 동작 지정

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, forwardRef } from "react";
 import {
   DescriptionStyle,
   InputStyle,
@@ -6,47 +6,100 @@ import {
   WrapperStyle,
 } from "./style";
 
-const Input = ({
-  type,
-  label,
-  placeholder,
-  description,
-  isError,
-  onChangeHandler,
-  onFocusHandler,
-  value,
-  onKeyPress,
-}) => {
-  if (!type) type = "text";
-  if (!label) label = "";
-  if (!onFocusHandler) onFocusHandler = () => {};
-  const [isFocused, setIsFocused] = useState(false);
+const Input = forwardRef(
+  (
+    {
+      type,
+      label,
+      placeholder,
+      description,
+      isError,
+      onChangeHandler,
+      onFocusHandler,
+      value,
+      onKeyPress,
+      ...props
+    },
+    ref = null
+  ) => {
+    if (!type) type = "text";
+    if (!label) label = "";
+    if (!onFocusHandler) onFocusHandler = () => {};
+    const [isFocused, setIsFocused] = useState(false);
 
-  return (
-    <WrapperStyle isFocused={isFocused} isError={isError}>
-      <LabelStyle isFocused={isFocused || value} isError={isError}>
-        {label}
-      </LabelStyle>
-      <InputStyle
-        type={type}
-        value={value}
-        placeholder={isFocused ? placeholder : ""}
-        isError={isError}
-        onChange={onChangeHandler}
-        onKeyPress={onKeyPress}
-        onFocus={() => {
-          setIsFocused(true);
-          onFocusHandler();
-        }}
-        onBlur={() => {
-          setIsFocused(false);
-        }}
-      />
-      {description && (
-        <DescriptionStyle isError={isError}>{description}</DescriptionStyle>
-      )}
-    </WrapperStyle>
-  );
-};
+    return (
+      <WrapperStyle isFocused={isFocused} isError={isError}>
+        <LabelStyle isFocused={isFocused || value} isError={isError}>
+          {label}
+        </LabelStyle>
+        <InputStyle
+          type={type}
+          value={value}
+          placeholder={isFocused ? placeholder : ""}
+          isError={isError}
+          onChange={onChangeHandler}
+          onKeyPress={onKeyPress}
+          onFocus={() => {
+            setIsFocused(true);
+            onFocusHandler();
+          }}
+          onBlur={() => {
+            setIsFocused(false);
+          }}
+          ref={ref}
+          {...props}
+        />
+        {description && (
+          <DescriptionStyle isError={isError}>{description}</DescriptionStyle>
+        )}
+      </WrapperStyle>
+    );
+  }
+);
+
+// const Input = ({
+//   type,
+//   label,
+//   placeholder,
+//   description,
+//   isError,
+//   onChangeHandler,
+//   onFocusHandler,
+//   value,
+//   onKeyPress,
+//   ...props
+// }) => {
+//   if (!type) type = "text";
+//   if (!label) label = "";
+//   if (!onFocusHandler) onFocusHandler = () => {};
+//   const [isFocused, setIsFocused] = useState(false);
+
+//   return (
+//     <WrapperStyle isFocused={isFocused} isError={isError}>
+//       <LabelStyle isFocused={isFocused || value} isError={isError}>
+//         {label}
+//       </LabelStyle>
+//       <InputStyle
+//         type={type}
+//         value={value}
+//         placeholder={isFocused ? placeholder : ""}
+//         isError={isError}
+//         onChange={onChangeHandler}
+//         onKeyPress={onKeyPress}
+//         onFocus={() => {
+//           setIsFocused(true);
+//           onFocusHandler();
+//         }}
+//         onBlur={() => {
+//           setIsFocused(false);
+//         }}
+//         {...props}
+//       />
+//       {description && (
+//         <DescriptionStyle isError={isError}>{description}</DescriptionStyle>
+//       )}
+//     </WrapperStyle>
+//   );
+// };
 
 export default Input;
