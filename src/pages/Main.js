fb4c121a67a4fd105b401bafd8dd595cae33c84b
alicehrk <81912837+alicehrk@users.noreply.github.com>
2024-01-18 16:37:57 +0900

    fix: 카테고리 수정 시 default value 설정

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -14,7 +14,7 @@ import Locker from "./Locker";
 const Main = () => {
   const [tab, setTab] = useState("내가 쓴");
   const [bgColor, setBgColor] = useState("#7BAB6E");
-  const [inputValue, setInputValue] = useState("");
+
   const [showCategorySetting, setShowCategorySetting] = useState(false);
 
   const [showSubModal, setShowSubModal] = useState(false);
@@ -24,6 +24,7 @@ const Main = () => {
   const [categoryInputValue, setCategoryInputValue] = useState(0);
   const [dotIconClicked, setDotIconClicked] = useState(false);
   const [clickedCategoryData, setClickedCategoryData] = useState({});
+  const [inputValue, setInputValue] = useState("");
   const tabType = tab === "내가 쓴" ? "personal" : "shared";
   const [isOverlayOn, setIsOverlayOn] = useState(false);
   const categoryState = useCategory(tabType);
@@ -51,6 +52,10 @@ const Main = () => {
     "#507247",
   ];
   const [selectedColor, setSelectedColor] = useState(ColorList[0]);
+
+  useEffect(() => {
+    setInputValue(clickedCategoryData.name);
+  }, [clickedCategoryData]);
 
   useEffect(() => {
     if (ColorList.includes(clickedCategoryData.color)) {
@@ -279,7 +284,6 @@ const Main = () => {
           <Input
             type="text"
             value={inputValue}
-            placeholder={clickedCategoryData.name}
             onChange={handleInputChange}
             onClick={handleInnerClick}
           />
@@ -300,7 +304,7 @@ const Main = () => {
                   type: tabType,
                 })
               }
-            ></ButtonV2>
+            >카테고리 수정</ButtonV2>
           </FixedButtonContainer>
           <ColorPalette>
             {ColorList.map((color) => (
