6ac559dd7f787c76e5df5d84ae5abf9bc6fd3d7e
graS2e <77924292+graS2e@users.noreply.github.com>
2024-01-19 11:19:08 +0900

    feat: 기도 수정하기 기능 추가(날짜 맨 처음 업데이트가 안 되는 오류 존재)

--- src/components/SelectDate/SelectDate.js
+++ src/components/SelectDate/SelectDate.js
@@ -6,6 +6,7 @@ import Calender from "../Calender/Calender";
   props 넘겨받을 목록 (History.js 파일 참고하기)
   1. setUpdateDate 변수 (api 호출용 날짜 데이터 저장)
   2. showSubModal 변수 (현재 컴포넌트 창 켜져있는지)
+  3. date 변수 (기존 선택되어야 하는 날짜)
 */
 
 const SelectDate = (props) => {
@@ -14,6 +15,12 @@ const SelectDate = (props) => {
   const [designedDate, setDesignedDate] = useState(null); // yyyy-mm-dd (요일) 형태
   const [selectedDate, setSelectedDate] = useState(null);
   const [showDatePicker, setShowDatePicker] = useState(false);
+
+  useEffect(() => {
+    if (props.date !== null) {
+      props.setUpdateDate(props.date);
+    }
+  }, []);
 
   const onClickCalendar = () => {
     if (selectedBtn === "calendar") {
