d4d405a54822c256d72cfa626835dc5133ca56b2
graS2e <77924292+graS2e@users.noreply.github.com>
2024-02-09 17:54:27 +0900

    Revert "Feature/share"

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -13,7 +13,6 @@ import { useLocation } from "react-router-dom";
 import useFlutterWebview from "../hooks/useFlutterWebview";
 import { useShare } from "../hooks/useShare";
 import Locker from "./Locker";
-import ChangeCategoryOrder from "./ChangeCategoryOrder";
 
 const Main = () => {
   const [tab, setTab] = useState("내가 쓴");
@@ -23,10 +22,6 @@ const Main = () => {
 
   const [showSubModal, setShowSubModal] = useState(false);
   const [showModal, setShowModal] = useState(false);
-  const [showOption, setShowOption] = useState(false);
-  const [shareMode, setShareMode] = useState(false);
-  const [isLockerOverlayOn, setIsLockerOverlayOn] = useState(false);
-  const [isOrderOverlayOn, setIsOrderOverlayOn] = useState(false);
   const [prayInputValue, setPrayInputValue] = useState("");
   const [dateInputValue, setDateInputValue] = useState(null);
   const [categoryInputValue, setCategoryInputValue] = useState(0);
@@ -34,7 +29,7 @@ const Main = () => {
   const [clickedCategoryData, setClickedCategoryData] = useState({});
   const [inputValue, setInputValue] = useState("");
   const tabType = tab === "내가 쓴" ? "personal" : "shared";
-
+  const [isOverlayOn, setIsOverlayOn] = useState(false);
   const categoryState = useCategory(tabType);
   const prayState = usePray(tabType);
   const { refetchCategoryList } = categoryState;
@@ -141,7 +136,7 @@ const Main = () => {
   };
 
   const clickLocker = () => {
-    setIsLockerOverlayOn(true);
+    setIsOverlayOn(true);
   };
 
   const handleInputChange = (e) => {
@@ -400,9 +395,9 @@ const Main = () => {
           </ColorPalette>
         </CategorySetting>
       )}
-      {isLockerOverlayOn && (
-        <Overlay isOverlayOn={isLockerOverlayOn}>
-          <Locker setIsOverlayOn={setIsLockerOverlayOn} />
+      {isOverlayOn && (
+        <Overlay isOverlayOn={isOverlayOn}>
+          <Locker setIsOverlayOn={setIsOverlayOn} />
         </Overlay>
       )}
       {isOrderOverlayOn && (
@@ -430,7 +425,10 @@ const Main = () => {
             src="images/ic_main_order.svg"
             alt="main_order_icon"
             onClick={() => {
-              if (categoryList.length === 0) {setShowModal(true); return;}
+              if (categoryList.length === 0) {
+                setShowModal(true);
+                return;
+              }
               setIsOrderOverlayOn(true);
               setShowOption(false);
             }}
@@ -596,15 +594,4 @@ const ColorDrop = styled.div`
     display: ${(props) =>
       props.color === props.selectedColor ? "block" : "none"};
   }
-`;
-
-const OptionBtn = styled.img`
-  opacity: ${(props) => (props.isVisible ? 1 : 0)};
-  visibility: ${(props) => (props.isVisible ? "visible" : "hidden")};
-  position: fixed;
-  position: fixed;
-  bottom: ${(props) =>
-    props.isVisible ? `calc(80px + ${props.movingDistance}px)` : "80px"};
-  right: 20px;
-  transition: all 0.2s ease;
 `;
