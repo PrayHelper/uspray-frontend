6623b1b196e33c851def158ae8570f221973e905
Limeojin <limeojin363@gmail.com>
2024-02-11 17:50:59 +0900

    Feature/kakao naver api (#231)
    * feat: 마지막 이름입력 API 제외하고 작업 완료
    
    * fix: api에 헤더 넣고 나머지 연결
    
    * feat: 소셜로그인 - 신규유저 이름입력 API 수정, 기존유저를 위한 중간페이지 작성
    
    * fix: redirect url 수정('/' -> '/main')
    
    * del: 테스트용 코드(console, alert) 삭제
    
    ---------
    
    Co-authored-by: graS2e <77924292+graS2e@users.noreply.github.com>
    Co-authored-by: alicehrk <81912837+alicehrk@users.noreply.github.com>

---
+++ src/pages/SocialRedirecting.js
@@ -0,0 +1,38 @@
+// 소셜 로그인(기존 가입자) 진행 도중 토큰을 저장하고 홈으로 redirect시키기 위한 페이지(without any UI)
+
+import { useEffect } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
+import useAuthToken from "../hooks/useAuthToken";
+import useAuthorized from "../hooks/useAuthorized";
+
+const SocialRedirecting = () => {
+  const [searchParams] = useSearchParams();
+
+  const accessToken = searchParams.get("accessToken");
+  const refreshToken = searchParams.get("refreshToken");
+
+  const { setAccessToken, setRefreshToken } = useAuthToken();
+  const { setAutorized } = useAuthorized();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!!accessToken && !!refreshToken) {
+      setAccessToken(accessToken);
+      setRefreshToken(refreshToken);
+      setAutorized();
+      navigate("/main");
+    }
+  }, [
+    setAccessToken,
+    setRefreshToken,
+    setAutorized,
+    navigate,
+    accessToken,
+    refreshToken,
+  ]);
+
+  return <></>;
+};
+
+export default SocialRedirecting;
