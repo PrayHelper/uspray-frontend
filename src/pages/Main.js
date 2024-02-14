9ff6851814785dcfbea0c6a6932627288cc4da21
graS2e <77924292+graS2e@users.noreply.github.com>
2024-02-14 16:21:58 +0900

    fix: 메인화면 첫 로딩될 때 카테고리 선택 안 되는 현상 해결

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -136,6 +136,7 @@ const Main = () => {
 
   useEffect(() => {
     refetchPrayList();
+    setSelectedCategoryIndex(firstCategoryIndex);
   }, [categoryList]);
 
   const handleTabChange = (newTab) => {
