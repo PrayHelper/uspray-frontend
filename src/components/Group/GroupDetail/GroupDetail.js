406ddfd7b3ae5b272b5a14874744b48b3af8872c
graS2e <77924292+graS2e@users.noreply.github.com>
2024-02-21 02:48:38 +0900

    fix: 불러오기 안 뜨는 오류 수정

--- src/components/Group/GroupDetail/GroupDetail.js
+++ src/components/Group/GroupDetail/GroupDetail.js
@@ -26,6 +26,8 @@ const GroupDetail = ({ group, setShowGroupDetail }) => {
     useCategory(tab);
   const [selectedCategoryIndex, setSelectedCategoryIndex] =
     useState(firstCategoryIndex);
+  const [isPraySelected, setIsPraySelected] = useState(false);
+
   const [categoryRefIndex, setCategoryRefIndex] = useState(0);
   const categoryRef = useRef([]);
 
@@ -125,6 +127,7 @@ const GroupDetail = ({ group, setShowGroupDetail }) => {
           shareMode={shareMode}
           setShareMode={setShareMode}
           listHandler={onGroupPray}
+          setIsPraySelected={setIsPraySelected}
         />
       )}
     </Wrapper>
