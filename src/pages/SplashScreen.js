c7dd30780a9ae913e541604b077c5d36089c9ef5
jongbin26 <010jjj@naver.com>
2024-05-23 11:52:31 +0900

    fix: 기존 recoil 코드 jotai 변경 및 recoil 의존성 삭제

--- src/pages/SplashScreen.js
+++ src/pages/SplashScreen.js
@@ -1,52 +1,47 @@
 import styled from 'styled-components';
 import useAuthToken from '../hooks/useAuthToken';
-import { useRecoilState } from 'recoil';
 import useRefresh from '../hooks/useRefresh';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import {useLocation, useNavigate} from 'react-router-dom';
+import {useEffect} from 'react';
 import useAuthorized from '../hooks/useAuthorized';
 
-import LogoSVG from '../images/logo_white.svg'
+import LogoSVG from '../images/logo_white.svg';
 
 const SplashScreen = ({url}) => {
+  const {getRefreshToken} = useAuthToken();
+  const {refresh} = useRefresh();
+  const {setAutorized, setUnAuthorized} = useAuthorized();
+  const navigate = useNavigate();
 
-  const { getRefreshToken } = useAuthToken();
-  const { refresh } = useRefresh();
-  const {setAutorized, setUnAuthorized} = useAuthorized()
-  const navigate = useNavigate()
-
-  console.log(`splash screen url: ${url}`)
+  console.log(`splash screen url: ${url}`);
   useEffect(() => {
-
     const run = async () => {
-
-      const refreshToken = await getRefreshToken()
-      if (refreshToken === undefined || refreshToken === "") {
-        console.log("refresh token is empty")
-        setUnAuthorized()
-        navigate(url)
-        return
+      const refreshToken = await getRefreshToken();
+      if (refreshToken === undefined || refreshToken === '') {
+        console.log('refresh token is empty');
+        setUnAuthorized();
+        navigate(url);
+        return;
       }
 
       try {
-        await refresh()
-        setAutorized()
-        console.log("success to refresh token")
+        await refresh();
+        setAutorized();
+        console.log('success to refresh token');
       } catch (e) {
-        setUnAuthorized()
-        console.log("fail to refresh token")
+        setUnAuthorized();
+        console.log('fail to refresh token');
       }
 
-      if (url === "/") {
-        navigate("/main")
+      if (url === '/') {
+        navigate('/main');
       } else {
-        navigate(url)
+        navigate(url);
       }
-    }
+    };
 
-    run()
-  }, [])
-
+    run();
+  }, []);
 
   return (
     <BackgroundStyle>
@@ -60,13 +55,15 @@ const BackgroundStyle = styled.div`
   height: 100vh;
   display: flex;
   align-items: center;
-  justify-content: center;  
+  justify-content: center;
 
   /* 전체적인 배경색 조합 */
-  background: linear-gradient(167.98deg, rgba(0, 163, 255, 0) 0%, rgba(0, 163, 255, 0.4) 100%),
-              linear-gradient(135deg, #D6FFCB 0.01%, #75BD62 100%);
+  background: linear-gradient(
+      167.98deg,
+      rgba(0, 163, 255, 0) 0%,
+      rgba(0, 163, 255, 0.4) 100%
+    ),
+    linear-gradient(135deg, #d6ffcb 0.01%, #75bd62 100%);
 `;
-
-
 
 export default SplashScreen;
