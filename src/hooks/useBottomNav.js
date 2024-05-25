011ecc5df0340722d5fb084fa983282460822a94
jongbin26 <010jjj@naver.com>
2024-05-25 15:23:01 +0900

    fix: 코드 줄이기

--- src/hooks/useBottomNav.js
+++ src/hooks/useBottomNav.js
@@ -1,10 +1,9 @@
 import {useEffect} from 'react';
-import {atom, useAtomValue, useSetAtom} from 'jotai';
+import {atom, useAtom} from 'jotai';
 
 const useBottomNav = () => {
   const visibleAtom = atom(true);
-  const isVisible = useAtomValue(visibleAtom);
-  const setIsVisible = useSetAtom(visibleAtom);
+  const [isVisible, setIsVisible] = useAtom(visibleAtom);
   // 초기화: true
   useEffect(() => {
     setIsVisible(true);
