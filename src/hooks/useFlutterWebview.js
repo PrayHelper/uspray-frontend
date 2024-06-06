f4c6ab844a9399347c66781b4af4116f0cdedebb
graS2e <77924292+graS2e@users.noreply.github.com>
2024-06-06 14:14:57 +0900

    Update useFlutterWebview.js

--- src/hooks/useFlutterWebview.js
+++ src/hooks/useFlutterWebview.js
@@ -1,5 +1,4 @@
-import { useRef } from "react";
-import { useEffect } from "react";
+import { useRef, useState, useEffect } from "react";
 import useSleep from "./useSleep";
 
 const nil = { isnil: true };
@@ -116,6 +115,8 @@ const useShareLink = () => {
 };
 
 const useFlutterWebview = () => {
+  const [bridgeAvailable, setBridgeAvailable] = useState(false);
+  
   const isMobile = () => {
     
     //eslint-disable-next-line
@@ -124,11 +125,31 @@ const useFlutterWebview = () => {
     const isGetAuthTokenAvail = typeof FlutterStoreAuthToken !== "undefined" && typeof FlutterStoreAuthToken.postMessage === "function"
     //eslint-disable-next-line
     const isStoreAuthTokenAvail = typeof FlutterStoreAuthToken !== "undefined" && typeof FlutterStoreAuthToken.postMessage === "function"
-    
+
+    const isMobile = () => {
+    return bridgeAvailable;
+  };
+
+  useEffect(() => {
+    const checkBridge = () => {
+      if (typeof window.Bridge !== 'undefined') {
+        setBridgeAvailable(true);
+      }
+    };
+
+    // 페이지가 로드되었을 때 `checkBridge`를 실행합니다.
+    window.addEventListener('load', checkBridge);
+
+    // 컴포넌트가 언마운트될 때 이벤트 리스너를 정리합니다.
+    return () => {
+      window.removeEventListener('load', checkBridge);
+    };
+  }, []);
+
     if (isDeviceTokenAvail) {
       return true;
     } else {
-      return false;
+      return bridgeAvailable;
     }
     
 /*
